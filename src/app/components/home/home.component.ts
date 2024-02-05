922e975597cec52495f56389e7a98b78452ecb4e
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-02-05 21:46:23 +0100

    added some style to the home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+
+  @ViewChild('hiddenDiv', {static: false}) private hiddenDiv!: ElementRef<HTMLDivElement>;
+isHiddenDivScrolledIntoView!: boolean;
+
+images:any[]=[
+  {
+    title:"Single Chat",
+    description:"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.",
+    image:'assets/home/message1.png'
+  },
+  {
+    title:"Group chats",
+    description:"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.",
+    image:'assets/home/message2.webp'
+  },
+  {
+    title:"Chat requests",
+    description:"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.",
+    image:'assets/home/message3.jpg'
+  }
+]
+
+
+
+@HostListener('window:scroll', ['$event'])
+isScrolledIntoView(){
+  if (this.hiddenDiv){
+    const rect = this.hiddenDiv.nativeElement.getBoundingClientRect();
+    const topShown = rect.top >= 0;
+    const bottomShown = rect.bottom <= window.innerHeight;
+    this.isHiddenDivScrolledIntoView = topShown && bottomShown;
+if(this.isHiddenDivScrolledIntoView){
+this.hiddenDiv.nativeElement.classList.add('return')
+}else{
+  this.hiddenDiv.nativeElement.classList.remove('return')
+}
+  }
+}
 
 }
