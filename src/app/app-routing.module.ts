168ab46f4f13667ac63846b484c3c5952061a58c
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-02-05 19:27:16 +0100

    guard and environment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { AuthGuard } from './core/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+     loadChildren: () => import('./components/login/login.module').then(m => m.LoginModule)
+},
+ {
+  path:'home',
+   loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule)},
+{
+  path:'profilo/:id',
+   loadChildren: () => import('./components/profile/profile.module').then(m => m.ProfileModule), canActivate:[AuthGuard]},
+{
+  path:'chat',
+   loadChildren: () => import('./components/chat/chat.module').then(m => m.ChatModule)},
+{
+  path:'**',
+  component:NotFoundComponent
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
